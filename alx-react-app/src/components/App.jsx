c908dfd919002c33706d1abed869c2eebffea00b
Ezugwu Chinonso <charlesnonny04@gmail.com>
2024-08-18 01:10:51 +0100

    third welcome message commit

--- alx-react-app/src/components/App.jsx
+++ alx-react-app/src/components/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import WelcomeMessage from "./WelcomeMessage";
+import WelcomeMessage from "./WelcomeMessage.jsx";
 import './App.css'
 
 function App() {

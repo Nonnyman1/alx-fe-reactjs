3f466f0a69f6b1d484999445ef71ddacabd329f6
Ezugwu Chinonso <charlesnonny04@gmail.com>
2024-08-18 03:00:06 +0100

    second minor update

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -9,7 +9,6 @@ import Footer from './components/Footer.jsx';
 import UserProfile from './components/UserProfile.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>

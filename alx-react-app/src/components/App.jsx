dc63c58271c0853d547d2ad57714abd93c881833
Ezugwu Chinonso <charlesnonny04@gmail.com>
2024-08-18 01:02:48 +0100

    update again the welcome message

--- alx-react-app/src/components/App.jsx
+++ alx-react-app/src/components/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import WelcomeMessage from "./Welcome";
+import WelcomeMessage from "./WelcomeMessage";
 import './App.css'
 
 function App() {

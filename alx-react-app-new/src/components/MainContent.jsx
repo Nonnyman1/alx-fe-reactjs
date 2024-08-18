8e5172b296fc5a35d7ba99b9ba357741d635a5a9
Ezugwu Chinonso <charlesnonny04@gmail.com>
2024-08-18 03:16:26 +0100

    added inline css to the SRC Components

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 function MainContent () {
     return (
     <main>
-        <p>I love to visit New York, Paris, and Tokyo.</p>
+        <p style={{backgroundColor:'GrayText', fontFamily:'sans-serif', fontWeight:'bolder'}}>I love to visit New York, Paris, and Tokyo.</p>
     </main>
     );
 }

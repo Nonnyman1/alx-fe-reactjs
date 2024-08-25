402f9c7930e0b4d058b2650e66760527023e33c3
Ezugwu Chinonso <charlesnonny04@gmail.com>
2024-08-25 23:16:42 +0100

    commited first changes

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import WelcomeMessage from "./components/WelcomeMessage.jsx";
 import './App.css';
 import Header from './components/header.jsx';
 import MainContent from './components/MainContent.jsx';
 import Footer from './components/Footer.jsx';
 import UserProfile from './components/UserProfile.jsx';
+import Counter from "./components/Counter.jsx";
 
 function App() {
 
@@ -20,7 +19,9 @@ function App() {
         <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
         <MainContent />
         <Footer />
+        <Counter/>
       </div>
+
     </>
   )
 }
